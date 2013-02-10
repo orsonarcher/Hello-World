0476dadd2c15632e7f34dd9cc32a02a3d450b635
dalbemil <d.albemil@gmail.com>
2013-02-10 02:56:22 -0500

    vgfgy

--- HelloDavid/Form1.Designer.cs
+++ HelloDavid/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 47);
+            this.button1.Location = new System.Drawing.Point(104, 210);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
